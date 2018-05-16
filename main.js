7ecabf203c1572901c033baf5f3e8bb1fa824b8c
Ross Cooper <rossjamescooper@gmail.com>
2018-05-16 01:52:24 -0700

    Working on city search

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-const api = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=e8560a1109f936430203f88c4e09f8f1"//units=imperial //make sure it comes back in Fahrenheit
-import cityList from "./Files/cityList.json"
+const api = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=e8560a1109f936430203f88c4e09f8f1"//units=imperial //make sure it comes back in Fahrenheire
 
 
-
-function findCity(city) {
-
-return cityList.filter(item => item.name == city)
-
-}
-
-console.log(cityList)
