d70038f85021ba34e480582d43e0aa4520bc296a
Ross Cooper <rossjamescooper@gmail.com>
2018-05-19 13:36:41 -0700

    Successful AJAX request for Las Vegas weather horray

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-const api = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=e8560a1109f936430203f88c4e09f8f1"//units=imperial //make sure it comes back in Fahrenheire
 
 
+
+
+
+
+
+const api = "http://api.openweathermap.org/data/2.5/forecast?id=5506956&APPID=e8560a1109f936430203f88c4e09f8f1"//units=imperial //make sure it comes back in Fahrenheire
+
+
+function getWeather() {
+	fetch(api)
+  		.then(response => {
+  			if(response.status !== 200) {
+        		console.log(response.status);
+        		return;
+      		}
+      		response.json()
+  				.then(data => {
+   			 	console.log(data)
+  				})
+		})
+}
+
+console.log(getWeather())
