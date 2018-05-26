7e145c417f3aed4dd5403c4213198ae9d589de9a
Ross Cooper <rossjamescooper@gmail.com>
2018-05-26 14:53:27 -0700

    Working on iterating over data, able to target data according to time

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
+let temperature = document.getElementById("temp")
 
 
 
 
 
-
-const api = "http://api.openweathermap.org/data/2.5/forecast?id=5506956&APPID=e8560a1109f936430203f88c4e09f8f1"//units=imperial //make sure it comes back in Fahrenheire
+const api = "http://api.openweathermap.org/data/2.5/forecast?id=5506956&units=imperial&APPID=e8560a1109f936430203f88c4e09f8f1"//units=imperial //make sure it comes back in Fahrenheire
 
 
 function getWeather() {
@@ -17,9 +17,13 @@ function getWeather() {
       		}
       		response.json()
   				.then(data => {
-   			 	console.log(data)
+  				console.log(data.list.filter(item => item.dt == 1527541200))
   				})
 		})
 }
 
 console.log(getWeather())
+
+
+
+///apixu.com
