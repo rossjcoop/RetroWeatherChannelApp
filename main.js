a96dfcbd1fd7fcf8e5ad5e7924b54635aadd1bd7
Ross Cooper <rossjamescooper@gmail.com>
2018-05-27 15:40:06 -0700

    Dynamic data, yay

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ let temperature = document.getElementById("temp")
 
 
 
-const api = "http://api.openweathermap.org/data/2.5/forecast?id=5506956&units=imperial&APPID=e8560a1109f936430203f88c4e09f8f1"//units=imperial //make sure it comes back in Fahrenheire
+const api = "http://api.openweathermap.org/data/2.5/weather?id=5506956&units=imperial&APPID=e8560a1109f936430203f88c4e09f8f1"//units=imperial //make sure it comes back in Fahrenheire
 
 
-function getWeather() {
+function getWeather(city) {
 	fetch(api)
   		.then(response => {
   			if(response.status !== 200) {
@@ -17,7 +17,11 @@ function getWeather() {
       		}
       		response.json()
   				.then(data => {
-  				console.log(data.list.filter(item => item.dt == 1527541200))
+  				// console.log(data.list.filter(item => item.dt == 1527541200))
+  				console.log(data.main.temp)
+  				console.log(data)
+  				let temp = data.main.temp
+  				temperature.innerHTML = `${temp}`
   				})
 		})
 }
@@ -27,3 +31,5 @@ console.log(getWeather())
 
 
 ///apixu.com
+
+
