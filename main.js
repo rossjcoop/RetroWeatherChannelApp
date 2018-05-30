c8299f23881765db5c9705645da744cc33e2e123
Ross Cooper <rossjamescooper@gmail.com>
2018-05-29 18:49:34 -0700

    Added functions to convert wind direction in North, South, etc and a function to return calm when there is no wind

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 let temperature = document.getElementById("temp")
 let conditions = document.getElementById("cond")
+let ccGif = document.getElementById("ccGif")
+let wind = document.getElementById("wind")
 
 
 
@@ -23,9 +25,14 @@ function getWeather(city) {
   				console.log(data)
   				let temp = Math.round(data.main.temp)
   				let cond = data.weather.reduce(item => item == "main").main
+  				let windSpeed = returnCalm(Math.round(data.wind.speed))
+  				let windDir = getWindDirection(data.wind.deg)
 
   				temperature.innerHTML = `${temp}`
   				conditions.innerHTML = `${cond}`
+  				ccGif.innerHTML = `<img class="gif" src="./Images/CurrentConditions/${cond}.gif">`
+  				wind.innerHTML = `Wind: ${windDir} ${windSpeed}`
+
 
   				})
 		})
@@ -33,6 +40,36 @@ function getWeather(city) {
 
 console.log(getWeather())
 
+
+
+function getWindDirection(deg) { ///This figures out the correct direction based on meterological degrees
+	if(deg >= 361 || NaN) {
+		return "Err"
+	} else if(deg <= 22.5 || deg >= 337.6) {
+		return "N"
+	} else if(deg >= 22.6 && deg <= 68.5) {
+		return "NE"
+	} else if(deg >= 68.6 && deg <= 112.5) {
+		return "E"
+	} else if(deg >= 112.6 && deg <= 157.5) {
+		return "SE"
+	} else if(deg >= 157.6 && deg <= 202.5) {
+		return "E"
+	} else if(deg >= 202.6 && deg <= 247.5) {
+		return "SW"
+	} else if (deg >= 247.6 && deg <= 292.5) {
+		return "W"
+	} else if (deg >= 292.6 && deg <= 337.5) {
+		return "NW"
+	}
+}
+
+function returnCalm(speed) { ///Simple function to return "Calm" if there is no wind
+	if(speed = 0) {
+		return "Calm"
+	} else
+		return speed
+}
 
 
 ///apixu.com
