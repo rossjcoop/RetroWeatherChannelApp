6d1fbacf6beab1f610a53e00809b95613e602236
Ross Cooper <rossjamescooper@gmail.com>
2018-09-21 13:08:30 -0700

    Adding node to break up Javascript code

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-https://api.weather.gov/stations/KLAS/observations
+//NEW API! Weather.gov is far better.
+//https://api.weather.gov
+
+
+//https://api.weather.gov/stations/KLAS/observations////Current conditions as McCarrin Airport Las Vegas
+
+
+
+
+//https://api.weather.gov/gridpoints/VEF/122,97/stations Brings in stations in a 2.5k area of your coordinates.
+
+
+
+
+//First, browser will take your coordinates from your location and feed them into this api:
+//https://api.weather.gov/points/<lat, lon>
+
+
+
+window.onload = function() {
+  var startPos;
+  var geoSuccess = function(position) {
+    startPos = position;
+    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+  };
+  navigator.geolocation.getCurrentPosition(geoSuccess);
+};
